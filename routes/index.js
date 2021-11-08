5fc25e34dc822056642d9295884149141aafe778
Bian <brpasiliao@gmail.com>
2021-11-08 08:23:05 -0500

    04_06

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const speakersRoute = require("./speakers");
+const feedbackRoute = require("./feedback");
+
+const router = express.Router();
+
+module.exports = (params) => {
+  const { speakersService } = params;
+
+  router.get("/", async (request, response) => {
+    const topSpeakers = await speakersService.getList();
+    response.render("layout", { pageTitle: "Welcome", template: "index", topSpeakers });
+  });
+
+  router.use("/speakers", speakersRoute(params));
+  router.use("/feedback", feedbackRoute(params));
+
+  return router;
+};
---
+++ routes/speakers.js
@@ -0,0 +1,21 @@
+const express = require("express");
+// const SpeakerService = require("../services/SpeakerService");
+
+const router = express.Router();
+
+module.exports = (params) => {
+  const { speakersService } = params;
+
+  router.get("/", async (request, response) => {
+    const speakers = await speakersService.getList();
+    response.render("layout", { pageTitle: "Speakers", template: "speakers", speakers });
+  });
+
+  router.get("/:shortname", async (request, response) => {
+    const speaker = await speakersService.getSpeaker(request.params.shortname);
+    console.log(speaker);
+    response.render("layout", { pageTitle: "Speakers", template: "speakers-detail", speaker });
+  });
+
+  return router;
+};
